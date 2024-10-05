7a1217429a1d9ab4b75679d6a187c495a1f98cea
UdayPandey01 <pandeyuday113@gmail.com>
2024-10-06 02:05:11 +0530

    accForm

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,21 +1,43 @@
+import { z } from "zod";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { AccountForm } from "@/features/accounts/components/account-form";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { insertAccountSchema } from "@/db/schema";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
-    const {isOpen, onClose} = useNewAccount()
+  const { isOpen, onClose } = useNewAccount();
 
-    return (
-        <Sheet open ={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>
-                        New Account
-                    </SheetTitle>
-                    <SheetDescription>
-                        Create a new account to track your transactions
-                    </SheetDescription>
-                </SheetHeader>
-            </SheetContent>
-        </Sheet>
-    )
-}
+  const onnSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm
+          onSubmit={onnSubmit}
+          disabled={false}
+          defaultValues={{ name: "" }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};
